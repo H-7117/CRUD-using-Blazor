456d1f1b84e377fb4043aa1ff240e645ab33fb55
hamood <hamood_alattas@gmail.com>
2024-02-19 20:35:05 +0300

    Add project files.

---
+++ CRUD/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace CRUD
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public decimal? Amount { get; set; }
+    }
+}
